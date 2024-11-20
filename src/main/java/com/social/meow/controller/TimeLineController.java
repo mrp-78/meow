b51bffece3984a31b4e5257ac7f602e1b7d593da
mohammadreza pakzadian <pakzadianmrp@gmail.com>
2024-11-20 12:29:22 +0330

    feat: add timeline controller

---
+++ src/main/java/com/social/meow/controller/TimeLineController.java
@@ -0,0 +1,43 @@
+package com.social.meow.controller;
+
+import com.social.meow.model.Post;
+import com.social.meow.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/timeline")
+public class TimeLineController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping()
+    public ResponseEntity<List<Post>> getAllPosts(
+            @RequestParam(required = false) Long lastId) {
+        try {
+            List<Post> posts = new ArrayList<Post>();
+            if(lastId != null) {
+
+            }else{
+                Pageable first10 = PageRequest.of(0,10);
+                posts = postRepository.findAll(first10).getContent();
+            }
+
+            return new ResponseEntity<>(posts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
--- src/main/java/com/social/meow/repository/PostRepository.java
+++ src/main/java/com/social/meow/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package com.social.meow.repository;
 
 import com.social.meow.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  PostRepository extends JpaRepository<Post, Long> { }
+public interface  PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p WHERE p.id < :lastId ORDER BY p.id DESC")
+    Page<Post> findPostsBeforeLastId(Long lastId, Pageable pageable);
+}
