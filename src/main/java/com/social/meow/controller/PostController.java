d905fa8000ca2634d4d97fb62d8237ced60a485a
mohammadreza pakzadian <pakzadianmrp@gmail.com>
2024-11-24 19:14:19 +0330

    fix: config cache manager and fix some issues

--- src/main/java/com/social/meow/controller/PostController.java
+++ src/main/java/com/social/meow/controller/PostController.java
@@ -3,22 +3,26 @@ package com.social.meow.controller;
 import com.social.meow.model.Post;
 import com.social.meow.repository.PostRepository;
 import com.social.meow.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+
 
     @Autowired
     PostRepository postRepository;
+    @Autowired
     PostService postService;
 
     @GetMapping()
@@ -32,13 +36,14 @@ public class PostController {
 
             return new ResponseEntity<>(posts, HttpStatus.OK);
         } catch (Exception e) {
+            logger.error(e.toString());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Cacheable(value = "posts", key = "#id")
     @GetMapping("/{id}")
-    public ResponseEntity<Post> getTutorialById(@PathVariable("id") long id) {
+    public ResponseEntity<Post> getPostById(@PathVariable("id") long id) {
         try {
             Post post = postService.getPostById(id);
 
@@ -52,6 +57,7 @@ public class PostController {
         }
     }
 
+//    @CachePut(value = "posts", key = "#post.id")
     @PostMapping()
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         try{
@@ -63,23 +69,19 @@ public class PostController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Post> updateTutorial(@PathVariable("id") long id, @RequestBody Post post) {
-        Optional<Post> postData = postRepository.findById(id);
-
-        if (postData.isPresent()) {
-            Post _post = postData.get();
-            _post.setText(post.getText());
-            _post.setUserId(post.getUserId());
-            return new ResponseEntity<>(postRepository.save(_post), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Post> updatePost(@PathVariable("id") long id, @RequestBody Post post) {
+        try {
+            Post updatedPost = postService.updatePost(id, post);
+            return new ResponseEntity<>(updatedPost, HttpStatus.CREATED);
+        } catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
         try {
-            postRepository.deleteById(id);
+            postService.deletePost(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
